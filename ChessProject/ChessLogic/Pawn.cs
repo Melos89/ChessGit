1fb5321f3fb9050dc0bf6feb95ba97fc87299783
Jonas <Nicolausfugler_89@hotmail.com>
2014-10-29 08:20:15 +0100

    My first commit!
    Started up the project

---
+++ ChessProject/ChessLogic/Pawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    class Pawn
+    {
+
+        string name = "Farmer Bill";
+        string colour;
+        int value;
+       
+
+    }
+}
